306f46e00849fc419d807bd187f97468c8a15dd5
jnstllng <jan@teamstelling.de>
2021-05-16 10:42:42 +0200

    Update weewx_backup.sh
    Create backup in /home/pi before moving to remote location

--- weewx_backup.sh
+++ weewx_backup.sh
@@ -21,28 +21,32 @@ dest="/home/pi/Shares/Temp"
 day=$(date +%y%m%d)
 archive_file="weewx-$day.tgz"
 
-# Print start status message.
+# Print start status message
 echo
-echo "Backing up $backup_files1 to $dest/$archive_file"
-echo "Backing up $backup_files2 to $dest/$archive_file"
-echo "Backing up $backup_files3 to $dest/$archive_file"
-echo "Backing up $backup_files4 to $dest/$archive_file"
+echo "Backing up $backup_files1 to /home/pi/$archive_file"
+echo "Backing up $backup_files2 to /home/pi/$archive_file"
+echo "Backing up $backup_files3 to /home/pi/$archive_file"
+echo "Backing up $backup_files4 to /home/pi/$archive_file"
 echo
 
-# Backup the files using tar.
+# Backup the files using tar
 # Option 1 - no need to stop and restart weewx
-# tar czf $dest/$archive_file $backup_files2 $backup_files3 $backup_files4
-# sqlite3 $backup_files1 ".backup $dest/$backup_files1.backup"
+# tar czf /home/pi/$archive_file $backup_files2 $backup_files3 $backup_files4
+# sqlite3 $backup_files1 ".backup /home/pi/$backup_files1.backup"
 # Option 2 - stop and restart of weewx needed
-tar czf $dest/$archive_file $backup_files1 $backup_files2 $backup_files3 $backup_files4
+tar -czf /home/pi/$archive_file $backup_files1 $backup_files2 $backup_files3 $backup_files4
+echo
+echo "Backup of created in /home/pi"
 
-# Print end status message. Uncomment if done manually.
-# echo
-# echo "Backup finished"
-# echo
+# Move backup archive to destination (e.g. mounted network drive)
+# Option 1
+# mv /home/pi/$archive_file $dest/$archive_file
+# mv /home/pi/$backup_files1.backup $dest/$backup_files1.backup
+# Option 2
+mv /home/pi/$archive_file $dest/$archive_file
+echo
+echo "Backup of moved to $dest"
+echo
 
 # Restart weewx
 sudo /etc/init.d/weewx start
-
-# Long listing of files in $dest to check success. Uncomment if done manually.
-# ls -lh $dest
