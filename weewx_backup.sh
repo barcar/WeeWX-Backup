3327428efc02d8d59be5d33d9d20d217093baaec
barcar <barcar@users.noreply.github.com>
2022-01-29 21:58:18 +0000

    Update weewx_backup.sh

--- weewx_backup.sh
+++ weewx_backup.sh
@@ -6,7 +6,7 @@
 #####################################################################
 
 # Stop weewx daemon
-sudo /etc/init.d/weewx stop
+systemctl stop weewx
 
 # What to backup
 backup_files1="/var/lib/weewx/weewx.sdb"
@@ -15,7 +15,7 @@ backup_files3="/etc/weewx/skins"
 backup_files4="/usr/share/weewx/user"
 
 # Where to backup
-dest="/home/pi/Shares/Temp"
+dest="/mnt/data/backup/$HOSTNAME_weewx"
 
 # Create archive filename
 day=$(date +%y%m%d)
@@ -23,30 +23,30 @@ archive_file="weewx-$day.tgz"
 
 # Print start status message
 echo
-echo "Backing up $backup_files1 to /home/pi/$archive_file"
-echo "Backing up $backup_files2 to /home/pi/$archive_file"
-echo "Backing up $backup_files3 to /home/pi/$archive_file"
-echo "Backing up $backup_files4 to /home/pi/$archive_file"
+echo "Backing up $backup_files1 to $dest/$archive_file"
+echo "Backing up $backup_files2 to $dest/$archive_file"
+echo "Backing up $backup_files3 to $dest/$archive_file"
+echo "Backing up $backup_files4 to $dest/$archive_file"
 echo
 
 # Backup the files using tar
-# Option 1 - no need to stop and restart weewx
+# Option 1 - no need to stop and restart weewx 
 # tar czf /home/pi/$archive_file $backup_files2 $backup_files3 $backup_files4
 # sqlite3 $backup_files1 ".backup /home/pi/$backup_files1.backup"
 # Option 2 - stop and restart of weewx needed
-tar -czf /home/pi/$archive_file $backup_files1 $backup_files2 $backup_files3 $backup_files4
+tar -czf $dest/$archive_file $backup_files1 $backup_files2 $backup_files3 $backup_files4
 echo
-echo "Backup of created in /home/pi"
+echo "Backup of created in $dest"
 
 # Move backup archive to destination (e.g. mounted network drive)
 # Option 1
 # mv /home/pi/$archive_file $dest/$archive_file
 # mv /home/pi/$backup_files1.backup $dest/$backup_files1.backup
 # Option 2
-mv /home/pi/$archive_file $dest/$archive_file
-echo
-echo "Backup of moved to $dest"
-echo
+#mv /home/pi/$archive_file $dest/$archive_file
+#echo
+#echo "Backup of moved to $dest"
+#echo
 
 # Restart weewx
-sudo /etc/init.d/weewx start
+systemctl start weewx
